678d7453007e6fba9f6212ceb7a42148a501bc81
wolfborg <kingderekx@yahoo.com>
2014-11-23 00:56:05 -0500

    Combat - functions fixed

--- src/Combat.java
+++ src/Combat.java
@@ -3,47 +3,103 @@ import java.util.Scanner;
 
 public class Combat
 {
+	private boolean turn = true;
 	static Scanner keyboard = new Scanner(System.in);
+	static Random rand = new Random();
 	
-	public static boolean hitChance(Character attacker, Character target)
+	public Combat(Character player, Enemy enemy)
 	{
-		Random rand = new Random();
-		int randomNum = rand.nextInt(100)+1;
+		System.out.println("Player HP: "+player.getHealth());
+		System.out.println("Enemy HP: "+enemy.getHealth());
+		System.out.println();
+		
+		while(true){
+			if(this.turn){
+				attack(player, enemy);
+			}else{
+				attack(enemy, player);
+			}
+			
+			this.turn = !this.turn;
+			
+			if(player.getHealth()<=0){
+				death();
+				break;
+			}else if(enemy.getHealth()<=0){
+				victory();
+				break;
+			}
+			
+			System.out.println("Player HP: "+player.getHealth());
+			System.out.println("Enemy HP: "+enemy.getHealth());
+			System.out.println();
+		}
+	}
+	
+	public void attack(Character attacker, Character target)
+	{
+		int choice = 0;
+		
+		if(!this.turn){
+			choice = rand.nextInt(3)+1;
+		}else{
+			String[] attacks = attacker.getAttacks();
+			
+			for(int i=0;i<attacks.length;i++){
+				System.out.println((i+1)+". "+attacks[i]);
+			}
+			
+			do{
+				if(choice>attacks.length || choice<0){
+					System.out.println("Error: Not a valid attack");
+				}
+				
+				System.out.print("Enter attack number: ");
+				choice = keyboard.nextInt();
+				System.out.println();
+			}while(choice>attacks.length || choice<0);
+		}
+		
+		if(hitChance(attacker, target)){
+			System.out.println("Hit: "+attacker.getAttacks()[choice-1]);
+			damage(target, choice);
+		}else{
+			System.out.println("Miss");
+		}
+	}
+	
+	public boolean hitChance(Character attacker, Character target)
+	{
+		int randomHit = rand.nextInt(100)+1;
 		int speedBonus = (attacker.getSpeed() - target.getSpeed())/5;
 		
-		System.out.println("RandomNum: "+randomNum);
-		System.out.println("Player Speed: "+attacker.getSpeed());
-		System.out.println("Enemy Speed: "+target.getSpeed());
-		System.out.println("SpeedBonus: "+speedBonus);
-		System.out.println("Outcome: "+(randomNum+speedBonus));
-		
-		if(randomNum+speedBonus >= 50){
+		if(randomHit+speedBonus >= 50){
 			return true;
 		}else{
 			return false;
 		}
 	}
 	
-	public static int attack(String[] attack)
+	public void damage(Character target, int choice)
 	{
-		for(int i=0;i<attack.length;i++){
-			System.out.println((i+1)+". "+attack[i]);
-		}
-		
-		System.out.print("Enter attack number: ");
-		int choice = keyboard.nextInt();
-		
-		return choice;
+		target.setHealth(target.getHealth()-(choice*10));
+	}
+	
+	public void victory()
+	{
+		System.out.println("You won!");
+	}
+	
+	public void death()
+	{
+		System.out.println("You are dead.");
+		System.out.println("Game Over");
 	}
 	
 	public static void main(String[] args)
 	{
 		Character player = new Character("link","warrior");
-		Enemy enemy = new Enemy("orc","warrior",3,500,150,0,80,60,null,null,null,null);
-		
-		System.out.println(hitChance(player, enemy));
-		System.out.println();
-		
-		attack(player.getAttacks());
+		Enemy enemy = new Enemy("orc","warrior",3,80,150,0,80,60,null,null,null,null);
+		new Combat(player, enemy);
 	}
 }
