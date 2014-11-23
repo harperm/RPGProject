b14b322bebc82ed007719672df65b1da8baa2bb8
Mallory Harper <harperm@wit.edu>
2014-11-23 16:53:19 -0500

    updates to equipment

--- src/Equiment.java
+++ src/Equiment.java
@@ -6,6 +6,13 @@ public class Equiment {
 	private String size;
 	private String equipped[] = new String [5];
 	private String chest[] = new String [5];
+	private String stat;
+	private String stat1;
+	private String stat2;
+	private int effect;
+	private int effect1;
+	private int effect2;
+
 	
 	
 	//----------------------------------
@@ -13,17 +20,29 @@ public class Equiment {
 	
 	public Equiment(String name,String type, String stat, int effect){
 		this.name= name;
-		this.type= type;	
+		this.type= type;
+		this.stat= stat;
+		this.effect= 0;
 	}
 	
-	public Equiment(String name, String type, String stat1, int effect1, String stat2, int effect2){
+	public Equiment(String name, String type, String stat, int effect, String stat1, int effect1){
 		this.name= name;
-		this.type= type;		
+		this.type= type;
+		this.stat= stat;
+		this.effect= 1;
+		this.stat1=stat1;		
+		this.effect1= 2;
 	}
 	
-	public Equiment(String name, String type, String stat1, int effect1, String stat2, int effect2, String stat3, int effect3){
+	public Equiment(String name, String type, String stat, int effect, String stat1, int effect1, String stat2, int effect2){
 		this.name= name;
-		this.type= type;		
+		this.type= type;
+		this.stat= stat;
+		this.stat1= stat1;
+		this.stat2= stat2;
+		this.effect=1;
+		this.effect1=2;
+		this.effect2=3;
 	}
 	
 	
@@ -49,6 +68,31 @@ public class Equiment {
 			this.chest= newChest;
 		}
 		
+		public void setStat(String newStat){
+			this.stat= newStat;
+		}
+		
+		public void setStat1(String newStat1){
+			this.stat1= newStat1;
+		}
+		
+		public void setStat2(String newStat2){
+			this.stat2= newStat2;
+		}
+			
+		public void setEffect(int newEffect){
+			this.effect= newEffect;
+		}
+		
+		public void setEffect1(int newEffect1){
+			this.effect1= newEffect1;
+		}
+		
+		public void setEffect2(int newEffect2){
+			this.effect2= newEffect2;
+		}
+		
+
 		//-----------------------------------
 		// get statements
 		public String getName(){
@@ -71,7 +115,48 @@ public class Equiment {
 			return chest;
 		}
 		
+		public String getStat(){
+			return stat;
+		}
+		
+		public String getStat1(){
+			return stat1;
+		}
+		
+		public String getStat2(){
+			return stat2;
+		}
+			
+		public int getEffect(){
+			return effect;
+		}
+		
+		public int getEffect1(){
+			return effect1;
+		}
+		
+		public int getEffect2(){
+			return effect2;
+		}
+		
+
+		//----------------------------------
+		//Effects players overall Stats
+		public void printCharacter()
+		{
+			System.out.println("Name: "+this.getName());
+			System.out.println("Type: "+this.getType());
+			System.out.println("Size: "+this.getSize());
+			System.out.println("Stat 1: "+this.getStat());
+			System.out.println("Effect 1: "+this.getEffect());
+			System.out.println("Stat 2: "+this.getStat1());
+			System.out.println("Effect 2: "+this.getEffect1());
+			System.out.println("Stat 3: "+this.getStat2());
+			System.out.println("Effect 3: "+this.getEffect2());
+		}
+ 
 		//-----------------------------------
+		//Keep/remove equipment
 		void equiped(){
 			
 		}
